f334dc86b532826f7ff843b576ac2c34b0a3d2b1
Documenter.jl <documenter@juliadocs.github.io>
2023-12-16 16:31:30 +0000

    build based on 27511ec

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Placar","category":"page"},{"location":"#Placar","page":"Home","title":"Placar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Placar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Placar]","category":"page"}]
+}
